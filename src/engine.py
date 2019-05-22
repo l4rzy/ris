18a78cc86d8961cef39b7263e7ffa3a39e5e939e
l4rzy <l4.foss@gmail.com>
2019-05-22 23:14:27 +0700

    returns results with limit

--- src/engine.py
+++ src/engine.py
@@ -4,51 +4,57 @@ from evals import Histogram
 
 class Engine:
     def __init__(self, datadir, dbdir = './'):
+        self.limit = 20
         self.datadir = datadir
         self.dbdir = dbdir
 
         self.db = DB(dbdir)
 
-    ## walk in datadir recursively to lookup for files, then calculate its
+    ## walks in datadir recursively to lookup for files, then calculate its
     ## vectors, save it to database
     def index(self, method):
         method.index(self.db, self.datadir)
 
-    ## get the value of picture
-    def getval(self, cat, path):
+    ## gets the value of picture
+    def getraw(self, cat, path):
         val = self.db.getraw(cat, path)
         return val
 
-    ## search the
-    def search(self, path, cat = 'histogram', limit = 10):
-        val = Histogram.calc(path)
-        print(val)
+    ## sets search limit
+    def setlimit(self, limit):
+        self.limit = limit
 
-        ## loop all over the database to find the lowest dirrerences
-        data = self.db.getcat(cat)
-
-        for img in data:
-            val = self.db.get(cat + '/' + img)
-
-
+    ## looks for similar images
     def look(self, path, method):
-        target = self.getval('histogram', path)
         ddict = {}
+        target = self.getraw(method.name, path)
+
+        if target is None:
+            kp, target = method.calc(path)
 
         try:
             for (root, dirs, files) in os.walk(self.datadir):
                 for f in files:
                     if f.endswith('jpg') or f.endswith('png'):
                         path = os.path.join(root, f)
-                        val = self.db.getraw('histogram', path)
+                        val = self.getraw(method.name, path)
+                        if val is None:
+                            print(f'error getraw for {path}: {DB.path2md5(path)}')
+                            kp, val = method.calc(path)
 
                         ## then compare two values, save to a dict, and
                         ## get top n best closest values
-                        d = Histogram.distance(target, val)
+                        d = method.distance(target, val)
                         ddict[path] = d
 
         except Exception as e:
             raise e
 
+        results = {}
+        i = 1
         for k in sorted(ddict, key=ddict.get):
-            print(f'{k}: [{ddict[k]}]')
+            if i == self.limit:
+                return results
+
+            results[k] = ddict[k]
+            i += 1
